faeb85a91543e2f26e7ffb1d1a5df8e7c7fa1026
Tri Nhan Nguyen <ngtrnhan1205@gmail.com>
2020-10-30 23:50:39 -0400

    Add Python Script: add template

--- xml-split.py
+++ xml-split.py
@@ -1,8 +1,5 @@
 import fileinput
 
-content = ""
-dataPath = ""
-
 def containDataPath(line):
     line = line.strip()
     if ("android:pathData" in line):
@@ -12,14 +9,33 @@ def containDataPath(line):
 def extractDataPath(line):
     return line.split("\"", 1)[1].split("\"", 1)[0]
 
+def getFirstHalf(line):
+    return line.split("\"", 1)[0]
+
+def getSecondHalf(line):
+    return line.split("\"", 1)[1].split("\"", 1)[1]
+
 def main():
+    dataPath = ""
+    upperTemplate = ""
+    lowerTemplate = ""
+    foundDataPath = False
     for line in fileinput.input():
         if (containDataPath(line)):
+            foundDataPath = True
             dataPath = extractDataPath(line)
-            print(dataPath)
+            upperTemplate += getFirstHalf(line)
+            lowerTemplate += getSecondHalf(line)
+        else:
+            if foundDataPath:
+                lowerTemplate += line
+            else:
+                upperTemplate += line
+
+    outputFile = open('output', 'w')
+    outputFile.write(upperTemplate)
+    outputFile.write(lowerTemplate)
+    outputFile.close()
 
 main()
 
-outputFile = open('output', 'w')
-outputFile.write(content)
-outputFile.close()
