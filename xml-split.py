246652d87270720a1023b78dd37e3d73491db8ef
Tri Nhan Nguyen <ngtrnhan1205@gmail.com>
2020-10-30 23:34:35 -0400

    Add Python script: extract path data content

--- xml-split.py
+++ xml-split.py
@@ -1,6 +1,7 @@
 import fileinput
 
 content = ""
+dataPath = ""
 
 def containDataPath(line):
     line = line.strip()
@@ -8,10 +9,14 @@ def containDataPath(line):
         return True
     return False
 
+def extractDataPath(line):
+    return line.split("\"", 1)[1].split("\"", 1)[0]
+
 def main():
     for line in fileinput.input():
         if (containDataPath(line)):
-           print("yes") 
+            dataPath = extractDataPath(line)
+            print(dataPath)
 
 main()
 
