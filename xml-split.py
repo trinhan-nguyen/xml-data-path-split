19ec9f1b4987603b7f98bc377d54102bc99817d5
Tri Nhan Nguyen <ngtrnhan1205@gmail.com>
2020-11-05 23:15:37 -0500

    Add Python Script: split pathData content

--- xml-split.py
+++ xml-split.py
@@ -15,27 +15,35 @@ def getFirstHalf(line):
 def getSecondHalf(line):
     return line.split("\"", 1)[1].split("\"", 1)[1]
 
+def splitPathData(pathData):
+    return pathData.split("M")
+
+def writeToFile(content):
+    outputFile = open('output', 'w')
+    for item in content:
+        outputFile.write(item)
+    outputFile.close()
+
 def main():
     pathData = ""
     upperTemplate = ""
     lowerTemplate = ""
     foundPathData = False
+    paths = []
     for line in fileinput.input():
         if (containPathData(line)):
             foundPathData = True
             pathData = extractPathData(line)
             upperTemplate += getFirstHalf(line)
             lowerTemplate += getSecondHalf(line)
+            paths = splitPathData(pathData)
         else:
             if foundPathData:
                 lowerTemplate += line
             else:
                 upperTemplate += line
+    writeToFile(paths)
 
-    outputFile = open('output', 'w')
-    outputFile.write(upperTemplate)
-    outputFile.write(lowerTemplate)
-    outputFile.close()
 
 main()
 
